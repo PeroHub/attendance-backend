690af5679c54b53c814ffd564deca9b09d11d523
PeroHub <65111015+PeroHub@users.noreply.github.com>
2025-11-20 13:14:14 +0100

    please work

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -9,7 +9,10 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    // service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, 
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
